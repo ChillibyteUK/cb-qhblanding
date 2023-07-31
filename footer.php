b2d8f65a5bec0ebff9ec2c0cec8e2c99548527b3
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-07-31 15:03:47 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,7 +5,8 @@ defined('ABSPATH') || exit;
 <footer>
     <div class="container-xl">
         <div>Copyright &copy; <?=date('Y')?>
-            Quick House Buyers. All Rights Reserved.</div>
+            Quick House Buyers.</div>
+            <?php /*
         <div><a href="/privacy-policy/">Privacy Policy</a> | <a href="/cookie-policy/">Cookie Policy</a> | <a
                 href="/complaints-procedure/">Complaints Procedure</a> | <a href="/opt-out/">Opt Out Marketing</a></div>
         <div>This is a sponsored article and is intended as an advertisement, not editorial content</div>
@@ -13,6 +14,7 @@ defined('ABSPATH') || exit;
             by Facebook in any way. FACEBOOK is a trademark of FACEBOOK, Inc.</div>
         <div>Quick House Buyer is a trading style of xxx Marketing Group Limited. Registered in England, Company Number:
             00000000. ICO Number: 00000000. 123 Building Place, Suburb, Town, AB12 3YZ</div>
+            */ ?>
 </footer>
 
 <?php
