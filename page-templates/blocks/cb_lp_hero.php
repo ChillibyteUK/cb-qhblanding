ebbad370a0c7c43eb159a4540aeb77e4b8d882c7
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-07-31 14:58:28 +0100

    hero change

--- page-templates/blocks/cb_lp_hero.php
+++ page-templates/blocks/cb_lp_hero.php
@@ -2,8 +2,9 @@
 $bg = wp_get_attachment_image_url(get_field('background'), 'full');
 ?>
 <div class="lp_hero" style="background-image:url(<?=$bg?>)">
+    <div class="overlay"></div>
     <img class="lp_hero__logo"
-        src="<?=get_stylesheet_directory_uri()?>/img/qhb-logo--blue.svg"
+        src="<?=get_stylesheet_directory_uri()?>/img/qhb-logo--wo.svg"
         width=3171 height=1557>
     <div class="container-xl">
         <h1><span><?=get_field('title')?></span>
