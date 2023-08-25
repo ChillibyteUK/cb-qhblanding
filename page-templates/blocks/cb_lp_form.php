9bdf38a62372bbc9077c5db84164cba900cb7cc1
CheechRockwizard <github@lamcat.co.uk>
2023-08-25 10:13:46 +0100

    Update cb_lp_form.php
    trigger gtm event on success

--- page-templates/blocks/cb_lp_form.php
+++ page-templates/blocks/cb_lp_form.php
@@ -3,35 +3,50 @@ $classes = $block['className'] ?? null;
 $fid = get_field('form_id');
 ?>
 <div class="form_container py-5 <?=$classes?>">
-    <div class="form_inner">
-        <?=do_shortcode('[gravityform id="' . $fid . '" ajax="true" title="false"]')?>
-    </div>
+  <div class="form_inner">
+    <?=do_shortcode('[gravityform id="' . $fid . '" ajax="true" title="false"]')?>
+  </div>
 </div>
 <script>
+  // if ptype input has value
+  const radioButtons = document.querySelectorAll('input[name="input_3"]');
+  let isChecked = false;
 
-    // if ptype input has value
-    const radioButtons = document.querySelectorAll('input[name="input_3"]');
-    let isChecked = false;
-
-    for (const radioButton of radioButtons) {
-      if (radioButton.checked) {
-        isChecked = true;
-        console.log('has value');
-        document.getElementById('gform_next_button_<?=$fid?>_1').click();
-        break;
-      }
+  for (const radioButton of radioButtons) {
+    if (radioButton.checked) {
+      isChecked = true;
+      console.log('has value');
+      document.getElementById('gform_next_button_<?=$fid?>_1').click();
+      break;
     }
-    // advance to next step
+  }
+  // advance to next step
 
-    add_image('label_<?=$fid?>_3_0','detatched');
-    add_image('label_<?=$fid?>_3_1','semi');
-    add_image('label_<?=$fid?>_3_2','terraced');
-    add_image('label_<?=$fid?>_3_3','bungalow');
-    add_image('label_<?=$fid?>_3_4','flat');
-    add_image('label_<?=$fid?>_3_5','other');
+  add_image('label_<?=$fid?>_3_0', 'detatched');
+  add_image('label_<?=$fid?>_3_1', 'semi');
+  add_image('label_<?=$fid?>_3_2', 'terraced');
+  add_image('label_<?=$fid?>_3_3', 'bungalow');
+  add_image('label_<?=$fid?>_3_4', 'flat');
+  add_image('label_<?=$fid?>_3_5', 'other');
 
-    function add_image(id,icon) {
-        const btn = document.getElementById(id).innerHTML;
-        document.getElementById(id).innerHTML = '<img src="<?=get_stylesheet_directory_uri()?>/img/icon-' + icon + '.svg" width=132 height=123 class="ptype-icon">' + btn;
-    }
+  function add_image(id, icon) {
+    const btn = document.getElementById(id).innerHTML;
+    document.getElementById(id).innerHTML =
+      '<img src="<?=get_stylesheet_directory_uri()?>/img/icon-' + icon +
+      '.svg" width=132 height=123 class="ptype-icon">' + btn;
+  }
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var forms = document.querySelectorAll('.gform_wrapper');
+
+    forms.forEach(function(form) {
+      form.addEventListener('gform_confirmation_loaded', function(event) {
+        console.log('success');
+        dataLayer.push({
+          'event': 'lead'
+        });
+      });
+    });
+  });
 </script>
