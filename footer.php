a57dd73f18fbe41f287bbb777f8aef78f89d7571
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-08-30 13:21:00 +0100

    added napb logo

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@ defined('ABSPATH') || exit;
 ?>
 <footer>
     <div class="container-xl">
+        <div class="text-center"><img src="<?=get_stylesheet_directory_uri()?>/img/NAPB.png" width=312 height=161 alt="NAPB"></div>
         <div class="py-3">Copyright &copy; <?=date('Y')?>
             Quick House Buyers.</div>
             <?php /*
